47ae22fe684f60ad6d7d9ab23fb5a5644e147f69
Steve Fan <sf200212345@gmail.com>
2024-04-13 15:09:54 -0400

    add db connection and reorganize file

--- app.py
+++ app.py
@@ -1,17 +1,82 @@
 import flask
 import flask_cors
-from google_api import *
+import sqlite3
+import pathlib
+import os
+import google.generativeai as genai
+
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+print("GOOGLE_API_KEY:", GOOGLE_API_KEY)
 
 
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
 
-# add all api routes here
-app.add_url_rule("/test-google", view_func=test_google, methods=["GET"])
-app.add_url_rule("/process-user-message", view_func=process_user_message, methods=["POST"])
-app.add_url_rule("/get-compare-list", view_func=get_compare_list, methods=["POST"])
-
 
 @app.route("/")
 def main():
     return flask.jsonify({"message": "you have reached the root of the backend :)"}), 200
+
+
+@app.route("/test-google", methods=["GET"])
+def test_google():
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content("Say something crazy")
+    return flask.jsonify({"message": response.text}), 200
+
+
+@app.route("/process-user-message", methods=["POST"])
+def process_user_message():
+    pass
+
+
+@app.route("/get-compare-list", methods=["POST"])
+def get_compare_list():
+    pass
+
+
+#------------------------------------------------------------------------------------------
+#--------------------------db connection stuff---------------------------------------------
+#------------------------------------------------------------------------------------------
+
+# everything below is taken straight from EECS 485
+# no need to touch this part
+def dict_factory(cursor, row):
+    """Convert database row objects to a dictionary keyed on column name.
+
+    This is useful for building dictionaries which are then used to render a
+    template.  Note that this would be inefficient for large queries.
+    """
+    return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
+
+
+def get_db():
+    """Open a new database connection.
+
+    Flask docs:
+    https://flask.palletsprojects.com/en/1.0.x/appcontext/#storing-data
+    """
+    if 'sqlite_db' not in flask.g:
+        db_filename = pathlib.Path(__file__).parent / "db.sqlite3"
+        flask.g.sqlite_db = sqlite3.connect(str(db_filename))
+        flask.g.sqlite_db.row_factory = dict_factory
+        # Foreign keys have to be enabled per-connection.  This is an sqlite3
+        # backwards compatibility thing.
+        flask.g.sqlite_db.execute("PRAGMA foreign_keys = ON")
+    return flask.g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close the database at the end of a request.
+
+    Flask docs:
+    https://flask.palletsprojects.com/en/1.0.x/appcontext/#storing-data
+    """
+    assert error or not error  # Needed to avoid superfluous style error
+    sqlite_db = flask.g.pop('sqlite_db', None)
+    if sqlite_db is not None:
+        sqlite_db.commit()
+        sqlite_db.close()
