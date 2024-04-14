596dbddc92cf588c82a981bd04715cffec5873c3
Steve Fan <sf200212345@gmail.com>
2024-04-14 13:39:09 -0400

    bugfix for compare

--- compare_helpers.py
+++ compare_helpers.py
@@ -15,7 +15,7 @@ def get_compare_list_db(request_body, connection):
     for factor in temp_factors:
         product_factors[factor['id']] = factor['factor_name']
 
-    output = []
+    all_products = []
 
     if request_body.get("product_ids") is not None:
         product_ids = request_body.get("product_ids")
@@ -26,57 +26,53 @@ def get_compare_list_db(request_body, connection):
                 "product_id": product['id'],
                 "name": product['product_code'],
             }
-            curr_product_factors = {}
-            for factor_name in product_factors.values():
-                curr_product_factors[factor_name] = {}
-
-            cursor = connection.execute("SELECT * FROM product_factor_rating WHERE product_id = ?", (product_id,))
-            product_factor_ratings = cursor.fetchall()
-            for rating in product_factor_ratings:
-                curr_factor_name = product_factors[rating['product_factor_id']]
-                curr_product_factors[curr_factor_name]["rating"] = rating['generated_rating']
-            
-            cursor = connection.execute("SELECT * FROM generated_product_factor WHERE product_id = ?", (product_id,))
-            generated_product_factors = cursor.fetchall()
-            for factor in generated_product_factors:
-                curr_factor_name = product_factors[factor['product_factor_id']]
-                curr_product_factors[curr_factor_name]["value"] = factor['generated_value']
-                curr_product_factors[curr_factor_name]["description"] = factor['generated_description']
-            
-
-            curr_product["attributes"] = [{"attributeName": name,"attributeValue": factor['value'], "attributeRating": factor['rating']} for name, factor in curr_product_factors.items()]
-
-            output.append(curr_product)
+            all_products.append(curr_product)
     else:
         cursor = connection.execute("SELECT * FROM product WHERE product_description_id = (SELECT product_description_id FROM message_thread WHERE id = ?)", (message_thread_id,))
         products = cursor.fetchall()
-        print(products, "products")
 
         for product in products:
             curr_product = {
                 "product_id": product['id'],
                 "name": product['product_code'],
             }
-            curr_product_factors = {}
-            for factor_name in product_factors.values():
-                curr_product_factors[factor_name] = {}
+            all_products.append(curr_product)
 
-            cursor = connection.execute("SELECT * FROM product_factor_rating WHERE product_id = ?", (product['id'],))
-            product_factor_ratings = cursor.fetchall()
-            for rating in product_factor_ratings:
-                curr_factor_name = product_factors[rating['product_factor_id']]
-                curr_product_factors[curr_factor_name]["rating"] = rating['generated_rating']
-            
-            cursor = connection.execute("SELECT * FROM generated_product_factor WHERE product_id = ?", (product['id'],))
-            generated_product_factors = cursor.fetchall()
-            for factor in generated_product_factors:
-                curr_factor_name = product_factors[factor['product_factor_id']]
-                curr_product_factors[curr_factor_name]["value"] = factor['generated_value']
-                curr_product_factors[curr_factor_name]["description"] = factor['generated_description']
-            
-            curr_product["attributes"] = [{"attributeName": name,"attributeValue": factor['value'], "attributeRating": factor['rating']} for name, factor in curr_product_factors.items()]
+    output = []
+    for product in all_products:
+        curr_product_factors = {}
+        for factor_name in product_factors.values():
+            curr_product_factors[factor_name] = {}
+        print(curr_product_factors, "curr_product_factors")
 
-            output.append(curr_product)
-    print(len(output), "length of output")
-    
+        product_id = product['product_id']
+
+        cursor = connection.execute("SELECT * FROM product_factor_rating WHERE product_id = ?", (product_id,))
+        product_factor_ratings = cursor.fetchall()
+        print(product_factor_ratings, "product_factor_ratings", product_id)
+        for rating in product_factor_ratings:
+            curr_factor_name = product_factors[rating['product_factor_id']]
+            curr_product_factors[curr_factor_name]["rating"] = rating['generated_rating']
+        
+        cursor = connection.execute("SELECT * FROM generated_product_factor WHERE product_id = ?", (product_id,))
+        generated_product_factors = cursor.fetchall()
+        for factor in generated_product_factors:
+            curr_factor_name = product_factors[factor['product_factor_id']]
+            curr_product_factors[curr_factor_name]["value"] = factor['generated_value']
+            curr_product_factors[curr_factor_name]["description"] = factor['generated_description']
+        print(curr_product_factors, "curr_product_factors")
+
+        local_output = []
+        for name, factor in curr_product_factors.items():
+            if not (factor.get("value") is None or factor.get("rating") is None or factor.get("description") is None):
+                local_output.append({
+                    "attributeName": name,
+                    "attributeValue": factor['value'],
+                    "attributeRating": factor['rating']
+                })
+
+        product["attributes"] = local_output
+
+        output.append(product)
+    print(len(output), "output length")
     return flask.jsonify({"products": output}), 200
